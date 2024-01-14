73cc6dc9d40b9dadab5538a9a178627988a155f2
Adil Ahamed <adilahamed189@gmail.com>
2024-01-14 15:36:54 +0530

    Add new pages and dependencies for backend & DB

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Profile } from './pages/Profile';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
+import { Header } from './components/Header';
+
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
